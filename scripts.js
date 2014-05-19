8f8153bfdce2d78a704e8b4345ff57102f110704
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-05-19 14:13:17 -0700

    solution

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
   
+  var url = "http://www.reddit.com/search.json?q=cats&limit=1";
 
+  var success = function(data) {
+    $('body').append("<h2>" + data.data.children[0].data.title + "</h2>");
+  };
+
+  var failure = function(jqXHR) {
+    alert("HTTP Status Code = " + jqXHR.status);
+  };
+
+  $.ajax(url, { type: 'get' }).success(success).fail(failure);
 });
