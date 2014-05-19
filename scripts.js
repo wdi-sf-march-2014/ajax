9385a6d73ef677cc298bb987ab197e592821d386
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-05-19 14:06:12 -0700

    solution

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-  
+
+  $( "#movie" ).submit(function( event ) {
+    
+    var movie_title = $('#movie_title').val();
+    var url = "http://www.omdbapi.com?t=" + movie_title;
+
+    $.ajax(url, {type: 'get'}).success(function(data) {
+      var movieData = JSON.parse(data);
+      $('#title').html(movieData.Title);
+      $('#movie_details').html("<p>Year:" + movieData.Year + "</p>"
+        + "<p>Director:" + movieData.Director + "</p>");
+    });
+
+    event.preventDefault();
+  });
 
 });
