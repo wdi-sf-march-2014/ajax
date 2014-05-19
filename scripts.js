7d540fe664ac296baacd067bc952e48eed54ddc1
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-05-19 14:16:32 -0700

    solution

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
   
+  var url = "http://www.omdbapi.com?i=tt1611224";
 
+  $.get(url).then(function(data) {
+      var movieData = JSON.parse(data);
+      alert(movieData.Title);
+    },
+   function(jqXHR) {
+      alert("You Got An Error, SON! " + jqXHR.status )
+   });
 });
